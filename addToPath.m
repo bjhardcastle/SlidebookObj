b49f0b348946e57c054dfda67539d4a467e61987
bjhardcastle <hardcastle@ucla.edu>
2020-04-20 21:01:06 -0700

    Added files not included initially

---
+++ addToPath.m
@@ -0,0 +1,29 @@
+function addToPath()
+%ADDTOPATH Adds folders required for SlidebookObj class to Matlab path
+%  addToPath()
+%
+if exist(fullfile(cd,'@SlidebookObj'),'dir')
+    addpath(cd);
+    addpath(fullfile(cd,'additionalfuncs'));
+else
+    disp('@SlidebookObj folder not found in current directory.')
+    return
+end
+
+% Prompt before saving path permanently
+disp('SlidebookObj class added to path temporarily.')
+disp('Save path to make it available next time Matlab opens?')
+response = '';
+while ~( strcmpi(response,'y') || strcmpi(response,'n') )
+    if ~isempty(response)
+        disp('Please enter ''y'' or ''n'' to choose yes or no.')
+    end
+    [response] = input('[y/n]: ','s');
+end
+
+if strcmpi(response,'y')
+    savepath
+    disp('Path saved.')
+else
+    disp('Path not saved. Run ''addToPath'' to use SlidebookObj class again.')
+end
